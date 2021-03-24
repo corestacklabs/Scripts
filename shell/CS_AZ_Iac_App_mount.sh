2090c9fb0fbfe6e8b8e0ef64564c77d481da4dd7
Eranachandran <eranachandran@corestack.io>
2021-03-24 13:09:58 +0530

    mount script changes

--- shell/CS_AZ_Iac_App_mount.sh
+++ shell/CS_AZ_Iac_App_mount.sh
@@ -4,6 +4,8 @@ mkdir -p /opt/core/cache
 
 (echo n; echo p; echo 1; echo 2048; echo;  echo w) | fdisk /dev/sdc)
 
-mkfs -t ext4 /dev/sdc1;cmp_cache_uuid=$(blkid -s UUID -o value /dev/sdc1)
+mkfs -t ext4 /dev/sdc1
+
+cmp_cache_uuid=$(blkid -s UUID -o value /dev/sdc1)
 
 echo "UUID=$cmp_cache_uuid /opt/core/cache/ ext4    defaults,nofail 0 0" >> /etc/fstab
